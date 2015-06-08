b64f7e380673208e7a59f9ba3d3e96fdff9f5919
yamagao <yamagao@126.com>
2015-06-08 10:24:09 -0400

    Initial code

---
+++ secondary-fc.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Foundation | Welcome</title>
+    <?php include 'include_meta.shtml'; ?>
+    
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/override-main.css" />
+    <link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+    <!--Scripts-->
+
+ <script src="foundation/js/vendor/modernizr.js"></script>
+ 
+
+<!--End Scripts-->
+   <!--Analytics-->
+<?php include 'include_analytics.shtml'; ?>
+<!--End Analytics-->
+  </head>
+  <body>
+  <!--==============================Top Navigation========================================-->
+<!--Top navigation dropdown-->
+<div class="row">
+     <?php include 'include_top_nav_drop_down.shtml'; ?> 
+</div>
+<!--END top navigation--> 
+ <div class="row">
+      <div id="banner" class="large-12 medium-12 columns" data-equalizer>
+     
+     <div class="large-9 medium-8 small-12 columns" data-equalizer-watch> <h1 title="Family"><strong>Family &amp; Community</strong></h1></div>
+     <div id="banner-photo" class="large-3 medium-4 small-4 columns" data-equalizer-watch> <img src="img/banner/family.png" width="231" height="150" alt="Family"></div>
+      </div>
+      </div>
+<div class="row">
+      <div id="content" class="large-12 medium-12 small-12 columns">
+      <?php include 'include_social_media.shtml'; ?> 
+      <p class="red-testing">&lt;!--START: HOME PAGE--&gt;</p>
+       <!--Slider replacement div for small screens-->
+    <div class="small-slider-replacement show-for-small-only large-12 medium-12 small-12columns">
+     <img src="img/banner/family.png" width="231" height="150" alt="Family">
+    </div>
+    <!--end slider replacement div for small screens-->
+      <h2>ABOUT</h2>
+    <!--Orbit slider-->
+
+    <div class="row">
+        <?php include 'include_slider.shtml'; ?>
+    </div>
+    <!--END orbit slider-->
+    <h2>Programs of Interest</h2>
+    <div class="row">
+  <div class="lower-content large-12 medium-12 small-12columns">
+  <div class="large-6 medium-6 small-12 columns">
+    <h3>Florida Association for Home and Community Education</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>Family Financial Management</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>Nutrition, Food Safety, &amp; Health</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>Youth Development &amp; 4-H</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+</div>
+  <div class="large-6 medium-6 small-12 columns">
+    <h3>Family Album Radio</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>Housing &amp; Community Development</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>Personal &amp; Family Well-Being</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+  </div>
+  </div>
+  </div> 
+    <p class="red-testing">&lt;!--END: HOME PAGE--&gt;</p>
+    <p class="red-testing">&lt;!--START: ABOUT/OVERVIEW PAGE--&gt;</p>
+    <!--START Top text only-->
+    <?php include 'include_social_media.shtml'; ?> 
+      <h2>&nbsp;</h2>
+     <div class="row">
+      
+     <div class="top-text-only large-12 medium-12 small-12 columns">
+     <h2>Family, Youth &amp; Consumer Sciences</h2>
+        <h3>Extension Programs</h3>
+        <a href="http://sfyl.ifas.ufl.edu"><img src="img/IFASExtWeb2013.png" width="350" alt="UF/IFAS Extension logo"></a>
+        <p>The Department of Family, Youth and Community Sciences offers you   information about health and nutrition, food safety, money management,   home concerns, relationships, and many other topics. Our resources can   help you whether you are retired, a first-time parent, a teen, or   someone in between.        </p>
+        <p>UF/IFAS Extension provides Floridians with life-long   learning programs in cooperation with county government, the United   States Department of Agriculture, and Florida A &amp; M University. </p>
+        <p>Search our Extension Program information for timely topics on <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/">Work &amp; Life,</a> <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/disaster-preparation/">Disaster Preparation</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/food-safety/">Food Safety</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/health-and-nutrition/">Health &amp; Nutrition</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/home-management/">Home Management</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/money-matters/">Money matters</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/relationships-and-family/">Relationships &amp; Family</a> and <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/community">Community</a>.</p>
+          <ul>
+            <li><a href="#">New list Second item</a></li>
+            <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+            <li><a href="#">Nullam congue cursus sollicitudin. Ut   tincidunt dapibus </a></li>
+            <li><a href="#">More resources...</a></li>
+          </ul>
+          <p>UF/IFAS Extension provides Floridians with life-long   learning programs in cooperation with county government, the United   States Department of Agriculture, and Florida A &amp; M University.          </p>
+     </div>
+    </div>
+    <div class="row">
+    <h2>Helpful Resources</h2>
+   <div class="lower-content2 large-12 medium-12 small-12 columns">
+  <div class="large-6 medium-6 small-12 columns">
+    <h3>Extension Programs</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus.<a href="#"> More...</a></p>
+    <h3>UF/IFAS Solutions</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>UF/IFAS Extension County (Alachua etc)</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+  </div>
+  <div class="large-6 medium-6 small-12 columns">
+    <h3>Extension Publications</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>FL County Extension Offices</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+    <h3>University of Florida Family, Youth, &amp; Consumer Sciences</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent   facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut   tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus   rhoncus. <a href="#">More...</a></p>
+  </div>
+  </div>
+  </div>
+  <p class="red-testing">&lt;!--END: ABOUT/OVERVIEW PAGE--&gt;</p>
+  <p class="red-testing">&lt;!--START: SAMPLE WORK &amp; LIFE &gt; RELATIONSHIP &amp; FAMILY PAGE--&gt;</p>
+    <div class="row">
+     <?php include 'include_social_media.shtml'; ?> 
+     <h2>TOPIC</h2>
+     <div class="no-left-right-padding top-static large-12 medium-12 small-12 columns" data-equalizer>
+     <div class="left-photo large-6 medium-12 small-12 columns" data-equalizer-watch><img src="img/100by650-place-holder2.jpg" alt="slide 1" /></div>
+     <div style="background-color:#490765;" class="right-text large-6 medium-12 small-12 columns" data-equalizer-watch>  <h3>Relationships &amp; Family</h3> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis nec nunc at volutpat. Nullam congue cursus sollicitudin. Ut tincidunt dapibus tempor. Cras rhoncus metus sit amet dui rhoncus rhoncus. </p>
+        <h4>Some of Our Favorite Family Resources</h4>
+         <ul>
+           <li><a href="#">New list Second item</a><a href="#"></a></li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+         <li><a href="#">Nullam congue cursus sollicitudin. Ut   tincidunt dapibus </a></li>
+         <li><a href="#">More resources...</a></li>
+          </ul></div>
+     </div>
+    </div>
+    <!--End Top text only-->
+ 
+ 
+  <!--Lower content V1-->
+  <div class="row"></div>
+  <!--END LOWER CONTENT V1-->
+  <!--START Lower content V2-->
+  <h2>20 RECENT STORIES</h2>
+  <div class="row">
+  <div class="wp-images-left lower-content large-12 medium-12 small-12 columns">
+   <?php 
+  $html="";
+  $url="http://blogs.ifas.ufl.edu/ifascomm/category/uf-ifas-communications/webteam/feedname/";
+   $url2="http://blogs.ifas.ufl.edu/ifascomm/category/uf-ifas-communications/webteam/";
+  include ('Wptoweb.php');
+  
+  
+    ?>
+
+  </div>
+  </div>
+  <!--END Lower Content V2-->
+   <p class="red-testing">&lt;!--END: SAMPLE WORK &amp; LIFE &gt; RELATIONSHIP &amp; FAMILY PAGE--&gt;</p>
+    <p class="red-testing">&lt;!--START: SAMPLE PROGRAM PAGE--&gt;</p>
+     <div class="row">
+     <?php include 'include_social_media.shtml'; ?> 
+     <h2>&nbsp;</h2>
+     <div  class="no-left-right-padding top-static large-12 medium-12 small-12 columns" data-equalizer>
+     <div class="left-photo large-6 medium-12 small-12 columns" data-equalizer-watch><img src="img/100by650-place-holder.jpg" alt="Program feature photo " /></div>
+     <div style="background-color:#844a35;" class="right-text large-6 medium-12 small-12 columns" data-equalizer-watch>  <h3><strong>Program: </strong>Nutrition, Food Safety &amp; Health</h3>
+       <h4>About</h4> 
+        <p>This can be the same BRIEF blurb about the program or you can use this as an alternating feature area for Programs of Interest.</p>
+        <h4>Some of Our Favorite Family Resources</h4>
+          <ul>
+           <li><a href="#">New list Second item</a><a href="#"></a></li>
+        <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+         <li><a href="#">Nullam congue cursus sollicitudin. Ut   tincidunt dapibus </a></li>
+         <li><a href="#">More resources...</a></li>
+          </ul></div>
+     </div>
+    </div>
+    <!--End Top text only-->
+ 
+ 
+  <!--Lower content V1-->
+  <div class="row"></div>
+  <!--END LOWER CONTENT V1-->
+  <!--START Lower content V2-->
+ 
+  <div class="row">
+  <div class="program-page-left-content large-6 medium-6 small-12 columns">
+   <h2>Programs of Interest</h2>
+   <h3>Listen</h3>
+ <p>Search our Extension Program information for timely topics on <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/">Work &amp; Life,</a> <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/disaster-preparation/">Disaster Preparation</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/food-safety/">Food Safety</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/health-and-nutrition/">Health &amp; Nutrition</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/home-management/">Home Management</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/money-matters/">Money matters</a>, <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/relationships-and-family/">Relationships &amp; Family</a> and <a href="http://blogs.ifas.ufl.edu/familyalbum/category/work-and-life/community">Community</a>.</p>
+          <ul>
+            <li><a href="#">New list Second item</a></li>
+            <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+            <li><a href="#">Nullam congue cursus sollicitudin. Ut   tincidunt dapibus </a></li>
+            <li><a href="#">More resources...</a></li>
+          </ul>
+          <h3>Food Safety</h3>
+<p>UF/IFAS Extension provides Floridians with life-long   learning programs in cooperation with county government, the United   States Department of Agriculture, and Florida A &amp; M University.          </p>
+  </div>
+  <div id="programs-right-wp" class="lower-content large-6 medium-6 small-12 columns">
+  
+   <h2> <a href="#"><img  class="rss" src="img/sm/rss.png" width="20" height="20" alt="rss feed"></a>Recent Stories</h2>
+  
+   <?php 
+  $html="";
+  $url="http://blogs.ifas.ufl.edu/ifascomm/category/uf-ifas-communications/webteam/feedname/";
+   $url2="http://blogs.ifas.ufl.edu/ifascomm/category/uf-ifas-communications/webteam/";
+  include ('Wptoweb-title-photo.php');
+  
+  
+    ?>
+
+  </div>
+  </div>
+  <!--END Lower Content V2-->
+   
+  </div>
+  </div>
+    <!--========================================Footer Area========================-->
+
+<!--Footer-->
+<div class="row">
+<div id="footer" class="large-12 medium-12 small-12 columns">
+  <?php include 'include_footer.shtml'; ?>
+  </div>
+</div>
+<!--END Footer-->
+<!--Bottom Background -->
+<div id="bottom-background"> </div>
+<!--End bottom background-->
+    <script src="foundation/js/vendor/jquery.js"></script>
+    <script src="foundation/js/foundation.min.js"></script>
+     <script src="foundation/js/foundation/foundation.orbit.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
