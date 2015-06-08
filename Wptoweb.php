db8e0f6ea07e2894e7002b284d0cab3b766e7705
yamagao <yamagao@126.com>
2015-06-08 14:08:23 -0400

    add placeholder if no post image

--- Wptoweb.php
+++ Wptoweb.php
@@ -1,9 +1,9 @@
 <?php
-$xml=simplexml_load_file($url);  
+$xml = simplexml_load_file($url);  
   
-$itemCount=$xml->channel->item->count();
+$itemCount = $xml->channel->item->count();
     
-if ($itemCount < 3){
+if($itemCount < 3){
 	$no=$itemCount;
 }  
 else{
@@ -17,19 +17,8 @@ for($i = 0; $i < $no; $i++){
 	$link=$xml->channel->item[$i]->link;
   
 	if($image == ""){
-		$wdth = 0;
-		$ht = 0;
-		$marginPadding = 0;
-		$display = "none";
+		$image = "img/wp-post-placeholder.jpg";
     }	
-    else{
-		$wdth = 425; 
-		$ht = 170; 
-		$marginPadding = 15; 
-		$display = "inherit";
-	}
-      
-  //$html .="<p><a href=\"$link\"><h1>$title</h1></a></p><img src=\"$image\" width=\"5%\" height=\"5%\"><h3> $description <a href=\"$link\">more</a></h3>";
   
 	echo "<div class='row'>
 			<div class='medium-6 columns'>
