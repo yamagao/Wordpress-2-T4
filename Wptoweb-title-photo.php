f7b704c2c7ff13a17757180745e521e4b2b66d1f
yamagao <yamagao@126.com>
2015-06-08 14:08:36 -0400

    reformat + add placeholder

--- Wptoweb-title-photo.php
+++ Wptoweb-title-photo.php
@@ -1,41 +1,24 @@
 <?php
-$xml=simplexml_load_file($url2);
-$xml=simplexml_load_file($url);
+$xml = simplexml_load_file($url);
   
-  
-  $itemCount=$xml->channel->item->count();
+$itemCount = $xml->channel->item->count();
     
-   if ($itemCount<3)
-  {
-	  $no=$itemCount;
-	  }  
-	  else {$no=3;}
-  for($i=0; $i<$no; $i++)
-  {	
-  $title=$xml->channel->item[$i]->title;
-  $description=$xml->channel->item[$i]->description;
-  
-  
-  $image=$xml->channel->item[$i]->image;
-  $link=$xml->channel->item[$i]->link;
-  
-  if ($image=="")
-  {
-      $wdth=0;
-      $ht=0;
-	  $marginPadding=0;
-	  $display="none";
-      }	
-      else {$wdth=425; $ht=170; $marginPadding=15; $display="inherit";}
-      
-  //$html .="<p><a href=\"$link\"><h1>$title</h1></a></p><img src=\"$image\" width=\"5%\" height=\"5%\"><h3> $description <a href=\"$link\">more</a></h3>";
-  
-  
- 
-  
-  echo        "
-  <div class='programs-wp-web'><a href='". $link. "'><h3>".$title."</h3></a><img style=' display:$display; ' src='".$image."'  ></div>";
- 	
+if($itemCount < 3){
+	$no = $itemCount;
+}  
+else{
+	$no = 3;
+}
 
+for($i = 0; $i < $no; $i++){	
+	$title = $xml->channel->item[$i]->title;
+	$image = $xml->channel->item[$i]->image;
+	$link = $xml->channel->item[$i]->link;
+  
+	if($image == ""){
+		$image = "img/wp-post-placeholder.jpg";
+    }
+	  
+	echo "<div class='programs-wp-web'><a href='". $link. "'><h3>".$title."</h3></a><img src='".$image."'></div>";
   }
 ?>
